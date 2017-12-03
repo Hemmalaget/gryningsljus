f726311b782e35c39512e4153df68ace24bfd20e
Daniel Ström <D@nielstrom.se>
2017-12-03 22:50:14 +0100

    Add back a smarter title overhang

--- app/components/sub-title.js
+++ app/components/sub-title.js
@@ -4,4 +4,8 @@ export default Title.extend`
   font-size: 1.2rem;
   font-weight: 500;
   margin-top: 2rem;
+
+  @media (min-width: 920px) {
+    margin-left: -1rem;
+  }
 `
--- app/components/title.js
+++ app/components/title.js
@@ -4,4 +4,8 @@ import styled from "styled-components"
 export default styled.h2`
   font-weight: 400;
   font-size: 2.2rem;
+
+  @media (min-width: 920px) {
+    margin-left: -1.2rem;
+  }
 `
